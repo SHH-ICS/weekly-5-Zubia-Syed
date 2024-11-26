0b86128d18e346d3f4b749deadd0689f3bdefa9e
Zubia Syed <Zubia.syed@stu.ocsb.ca>
2024-11-26 15:45:54 +0000

    Python3 changes

--- assignment.py
+++ assignment.py
@@ -1,6 +1,8 @@
 def calculatePi(digits):
-  result = 0
-  return result
-number_of_digits = input(int(input("enter iteration number: ")))
-pi = sum(4 * (-1) ** i / (2 * i + 1) for i in range(number_of_digits))
-print(calculatePi(number_of_digits))
+    result = sum(4 * (-1) ** i / (2 * i + 1) for i in range(digits))
+    return result
+
+number_of_digits = int(input("Enter iteration number: ")) 
+pi = calculatePi(number_of_digits)  
+print(pi)
+
